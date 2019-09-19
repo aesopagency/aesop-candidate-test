a04701d6ce0411f7ecf681082f05c20795161524
Laura Tombs <Laura Tombs>
2019-09-19 17:32:40 +0100

    Laura Tombs: candidate test Dancing Element

--- exercises/1-dancing-element/index.js
+++ exercises/1-dancing-element/index.js
@@ -1,21 +1,21 @@
-import { Component } from 'react'
+import { Component } from "react";
 
-import './style.scss'
+
+import "./style.scss";
 
 export default class DancingElementExercise extends Component {
-  componentDidMount() {
-    // Nextjs precompiles components to render in server side wherever possible,
-    // this means if you try to import libraries that work only on client-side
-    // it will fail to compile your page.
-    // (eg: if they interact with browser `window` global)
-
-    // However componentDidMount only triggers on client side
-    // so we can require our client-side only libraries in here
-    // for example: const jQuery = require('jquery')
-  }
 
   render() {
-    // Build your element here
-    return null
+    return (
+      <div className="dancingElement__container">
+        <div className="dancingElement__wrapper">
+          <div className="dancingElement">
+            <div className="dancingElement__copyContainer">
+              <h1 className="dancingElement__copy">Hello, world!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
